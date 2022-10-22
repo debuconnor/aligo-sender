bbd2975293c4bc95357b414c794a44b3e83fb991
debuconnor <gterppw@gmail.com>
2022-10-23 08:25:08 +0900

    return decoded json msg

--- api/parser.go
+++ api/parser.go
@@ -11,28 +11,21 @@ func IsJson(contentType string) bool {
 	return contentType == "application/json"
 }
 
-func DecodeJson(data *http.Request, obj struct{}) (result int) {
+func DecodeJson(data *http.Request, obj interface{}) interface{} {
 	var unmarshalErr *json.UnmarshalTypeError
 	_obj := obj
-	result = -1
 
-	log.Println(obj)
 	decoder := json.NewDecoder(data.Body)
 	decoder.DisallowUnknownFields()
 	err := decoder.Decode(&_obj)
-	log.Println(_obj)
+
 	if err != nil {
-		/*
-			Err list
-			1 : Wrong Type Field
-			2 : Bad Request
-		*/
 		if errors.As(err, &unmarshalErr) {
-			result = 1
+			log.Fatalln("Wrong Type Field")
 		} else {
-			result = 2
+			log.Fatalln("Bad Request")
 		}
 	}
 
-	return
+	return _obj
 }
